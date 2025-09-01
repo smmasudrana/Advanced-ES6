1da6f287725309cfe7d0f9264dfe58991e82ace4
smmasudrana <smmasudrana62@gmail.com>
2025-09-01 20:00:40 +0600

    add chain js

---
+++ chain.js
@@ -0,0 +1,15 @@
+const student = {
+    name: 'rafid',
+    marks: 90,
+    1: 50,
+    'home-address':'kochu khet',
+    family:{
+        title: 'bhuyiah',
+        // mother:{
+        //     name: 'noor jahan',
+        //     age: 45,
+        // }
+    }
+}
+
+console.log(student.family.mother?.age);
