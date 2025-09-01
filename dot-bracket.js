cdd15f03ef5536e82277ae291894e5004dab4e4b
smmasudrana <smmasudrana62@gmail.com>
2025-09-01 19:47:59 +0600

    add dont and bracket notaion

---
+++ dot-bracket.js
@@ -0,0 +1,15 @@
+const student = {
+    name: 'rafid',
+    marks: 90,
+    1: 50,
+    'home-address':'kochu khet',
+}
+//dot notation;
+const studenName = student.name;
+
+//bracket notation;
+const studentName2= student['1'];
+const studenName3 = student['home-address']
+console.log(studentName2, studenName3);
+
+
